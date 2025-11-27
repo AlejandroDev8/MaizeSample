6e9552e9af8f8675f4a188260f6abc3964e0d135
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-11-27 01:28:29 -0600

    Add back navigation and edit actions to ViewMaizeSample for improved user experience

--- app/Filament/Dashboard/Resources/MaizeSamples/Pages/ViewMaizeSample.php
+++ app/Filament/Dashboard/Resources/MaizeSamples/Pages/ViewMaizeSample.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Dashboard\Resources\MaizeSamples\Pages;
 
 use App\Filament\Dashboard\Resources\MaizeSamples\MaizeSampleResource;
+use Filament\Actions\Action;
 use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Support\Icons\Heroicon;
 
 class ViewMaizeSample extends ViewRecord
 {
@@ -13,7 +15,16 @@ class ViewMaizeSample extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            EditAction::make(),
+            Action::make('back')
+                ->label('Volver')
+                ->icon(Heroicon::OutlinedArrowLeft)
+                ->color('gray')
+                ->url($this->getResource()::getUrl('index')),
+            EditAction::make('edit')
+                ->label('Editar')
+                ->icon(Heroicon::OutlinedPencil)
+                ->color('primary')
+                ->url($this->getResource()::getUrl('edit', ['record' => $this->getRecord()])),
         ];
     }
 }
