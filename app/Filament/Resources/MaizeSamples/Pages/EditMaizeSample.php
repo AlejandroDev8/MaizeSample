bea0a1f7ad71b02084ae9a2543d7f0404d0a02ba
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-11-27 01:37:16 -0600

    Add success notification and improve header actions in EditMaizeSample for enhanced user feedback and navigation

--- app/Filament/Resources/MaizeSamples/Pages/EditMaizeSample.php
+++ app/Filament/Resources/MaizeSamples/Pages/EditMaizeSample.php
@@ -6,22 +6,41 @@ use App\Filament\Resources\MaizeSamples\MaizeSampleResource;
 use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Icons\Heroicon;
 
 class EditMaizeSample extends EditRecord
 {
     protected static string $resource = MaizeSampleResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title("La muestra de maíz {$this->record->name} ha sido actualizada exitosamente.")
+            ->body('La muestra de maíz ha sido actualizada exitosamente.');
+    }
+
     protected function getHeaderActions(): array
     {
         return [
-            ViewAction::make(),
-            DeleteAction::make(),
             Action::make('back')
-                ->label('Regresar')
-                ->url($this->getResource()::getUrl('index'))
-                ->icon('heroicon-o-arrow-left')
-                ->color('gray'),
+                ->label('Volver')
+                ->icon(Heroicon::OutlinedArrowLeft)
+                ->color('gray')
+                ->url($this->getResource()::getUrl('index')),
+            ViewAction::make('view')
+                ->label('Ver')
+                ->icon(Heroicon::OutlinedEye)
+                ->color('primary')
+                ->url($this->getResource()::getUrl('view', ['record' => $this->getRecord()])),
+            DeleteAction::make(),
         ];
     }
 }
