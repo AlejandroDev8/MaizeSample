9597874ed454b0c0c4aea79b2acebc764c865126
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-11-27 01:37:13 -0600

    Add success notification for MaizeSample creation to enhance user feedback

--- app/Filament/Resources/MaizeSamples/Pages/CreateMaizeSample.php
+++ app/Filament/Resources/MaizeSamples/Pages/CreateMaizeSample.php
@@ -3,9 +3,18 @@
 namespace App\Filament\Resources\MaizeSamples\Pages;
 
 use App\Filament\Resources\MaizeSamples\MaizeSampleResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateMaizeSample extends CreateRecord
 {
     protected static string $resource = MaizeSampleResource::class;
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title("Muestra de Maíz {$this->record->name} creada")
+            ->body('La muestra de maíz ha sido creada exitosamente.');
+    }
 }
