2135a926ece0c1673dd4e326211ca04a6b2bc0a5
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-12-04 12:52:11 -0600

    Add notification and redirect functionality to CreateFarmer page

--- app/Filament/Dashboard/Resources/Farmers/Pages/CreateFarmer.php
+++ app/Filament/Dashboard/Resources/Farmers/Pages/CreateFarmer.php
@@ -3,9 +3,23 @@
 namespace App\Filament\Dashboard\Resources\Farmers\Pages;
 
 use App\Filament\Dashboard\Resources\Farmers\FarmerResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateFarmer extends CreateRecord
 {
     protected static string $resource = FarmerResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('view', ['record' => $this->record]);
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title("Agricultor {$this->record->name} creado")
+            ->body('El agricultor ha sido creado exitosamente.');
+    }
 }
