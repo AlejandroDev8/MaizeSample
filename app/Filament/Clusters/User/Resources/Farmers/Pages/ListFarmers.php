1e16a594d8c375db9148ed431591d09e38fe7e90
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-12-14 16:50:24 -0600

    Comment out CreateAction in ListFarmers and ListMaizeSamples pages

--- app/Filament/Clusters/User/Resources/Farmers/Pages/ListFarmers.php
+++ app/Filament/Clusters/User/Resources/Farmers/Pages/ListFarmers.php
@@ -13,7 +13,7 @@ class ListFarmers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            // CreateAction::make(),
         ];
     }
 }
